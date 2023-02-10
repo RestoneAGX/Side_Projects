f98a13e47270b565aa3f7d66eda485c5b7ec61b1
RestoneAGX <redstonegameraa@gmail.com>
2023-02-09 19:47:52 -0500

    Starting to implement a word list generation feature

--- pictionary_thing/main.c
+++ pictionary_thing/main.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-#include<math.h>
-#include<rand.h>
+#include<stdlib.h>
+#include<time.h>
+
+int instances;
 
 int generate_num(int min, int max){
-    srand(time(0));
+    srand(time(0) * instances++);
     return (rand() % (max - min + 1)) + min;
 }
 
@@ -11,34 +13,42 @@ void help() {
     printf("");
 }
 
-void (*ftable[])() = {};
+void generate_list(FILE* fp){
+    fseek(fp, 0, SEEK_END);
+    int len = ftell(fp);
+    rewind(fp);
+    
+    char buff[len];
+    int newlines[255];
+    int i = 0, newline_count;
+    
+    for (; !feof(fp); i++){
+
+    }
+}
+
+void generate_word(){
+    char buff[100];
+    for(; !feof(fp); i++ ){
+        buff[i] = fgetc(fp);
+        if (buff[i] == '\n') {
+            if (generate_num(1, 4) == 2) break;
+            last_newline = i+1;
+        }
+    }
+
+    buff[i] = 0;
+    printf("Your Word is: %s\n", buff + last_newline);
+}
 
 int main(int argc, char** argv){
     if (argc < 2) return -1;
+    
     // check for help or list command
+    FILE* fp = fopen(argv[1], "r");
 
     
-    FILE* fp = fopen(argv[1], "r");
-    fseek(fp, 0, SEEK_END);
-    int len = ftell(fp);
-    char buff[len];
-    int last_newline = 0;
-    unsigned char newline_count = 0;
-    unsigned char target_count = generate_num(1, len);
-    char c;
-
-    for(int i = 0; !feof(fp); i++ ){
-        c = fgetc(fp);
-        if (c == '\n') {
-            last_newline = i;
-            if (generate_num == 1){
-                // write to stdout and say the word
-                fwrite
-            }
-        }
-    }
-    // randomly select one
-
-
+    // buff[len] = 0; 
+    // printf("%s\n", buff);
     return 0;
 }
