a148374c0d44da52177e4a5466930e49f7979420
Red <redstonegameraa@gmail.com>
2023-04-25 14:45:46 +0000

    Now able to win

--- tic-tac/main.c
+++ tic-tac/main.c
@@ -1,8 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define OR(slot, a,b,c)(slot[a] | slot[b] | slot[c])
+
 char slots[9] = {'1','2','3','4','5','6','7','8','9'};
 char plr = 'X';
+
+int checkForWin(char plr, int* slot){
+    return (OR(slot, 0, 1, 2) == plr) || (OR(slot, 0, 4, 8) == plr) || (OR(slot, 0, 3, 6) == plr) ||
+           (OR(slot, 2, 5, 8) == plr) || (OR(slot, 2, 4, 6) == plr) || 
+           (OR(slot, 6, 7, 8) == plr) || (OR(slot, 1, 4, 7) == plr) || (OR(slot, 3, 4, 5) == plr);
+}
 
 int main(){
     printf("Welcome To Tic-Tac-Toe\nType the number to select your spot & press q to quit\n\n");
@@ -22,6 +30,11 @@ int main(){
         if(slots[point-1] != 'X' && slots[point-1] != 'O'){
             plr = (plr == 'X') ? 'O' : 'X';
             slots[point-1] = plr;
+        }
+        
+        if (checkForWin(plr, slots)){
+            printf("\n%c Wins!!!\n", plr);
+            return 0;
         }
     }
     return 0;
